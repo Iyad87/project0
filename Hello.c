31fce3117e90a9c21477dc6acb95999ce948d352
Iyad87 <iyad0tata@gmail.com>
2018-10-20 22:57:19 +0200

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,20 @@
+include <stdio.h>
+
+int main(void){
+	
+	
+	int x = 10;
+	int y = 5;
+	
+	printf(" x is %i\n",x);
+		printf(" y is %i\n",y);
+		  // swaping code
+	 int temp = x; 
+	 x = y;
+	 y = temp;
+	 
+
+	
+	
+return 0;	
+}
