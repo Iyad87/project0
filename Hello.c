41bd42b92c2737a1a5267c01d8a3f5ce7e26dd05
Iyad Altoubah <iyad0tata@gmail.com>
2018-10-21 23:22:38 +0200

    add sum int

--- Hello.c
+++ Hello.c
@@ -5,6 +5,7 @@ int main(void){
 	
 	int x = 22;
 	int y = 5;
+	int sum = x *y;
 	
 	printf(" x is %i\n",x);
 		printf(" y is %i\n",y);
@@ -16,6 +17,8 @@ int main(void){
 
 	printf(" x is now %i\n",x);
     printf(" y is now %i\n",y);
+	printf("the sum is %i",sum);
+	
 	
 return 0;	
 }
